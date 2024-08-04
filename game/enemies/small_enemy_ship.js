01d4105b4bbce787f8aa805e180d57f69b057269
godzilla5hrimp <oleg.a.lavrov@protonmail.com>
2024-08-04 20:44:45 +0200

    fix enemy movement;

--- game/enemies/small_enemy_ship.js
+++ game/enemies/small_enemy_ship.js
@@ -1,9 +1,11 @@
 import { Enemy } from "./enemy.js"
 
+
 export class SmallEnemyShip extends Enemy {
     constructor(game) {
       super(game);
       this.x = 0;
       this.y = this.generateYPosition(this.x);
     }
+
 }
